2c54101f5a51a6a40159831ab45683874bb0e3ca
leonardobernardi <lbernardi4298@gmail.com>
2021-11-30 14:56:07 +0100

    bonus

--- src/it/biglietti/Biglietteria.java
+++ src/it/biglietti/Biglietteria.java
@@ -1,17 +1,24 @@
 package it.biglietti;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Biglietteria {
 
 	public static void main(String[] args) {
-		int km, eta;
+		int km, eta, flessibileInput;
+		boolean flessibile = false;
 		Scanner input = new Scanner(System.in);
 		System.out.print("Inserire km:");
 		km = input.nextInt();
 		System.out.print("Inserire eta: ");
 		eta = input.nextInt();
-		Biglietto biglietto = new Biglietto(km, eta);
+		System.out.print("Digitare 1 per acquistare un biglietto flessibile altrimenti digitare 2");
+		flessibileInput = input.nextInt();
+		if (flessibileInput == 1) {
+			flessibile = true;
+		}
+		Biglietto biglietto = new Biglietto(km, eta, LocalDate.now(), flessibile);
 		try {
 			biglietto.isValidKm();
 		}catch (Exception e) {
@@ -25,7 +32,10 @@ public class Biglietteria {
 		
 		input.close();
 		
-		System.out.println("$" + biglietto.calcolaPrezzo());
+		System.out.println("Prezzo: $" + biglietto.calcolaPrezzo());
+		System.out.println("Data di scadenza: " + biglietto.calcolaDataScadenza());
 	}
+
+	
 
 }
--- src/it/biglietti/Biglietto.java
+++ src/it/biglietti/Biglietto.java
@@ -3,18 +3,26 @@ package it.biglietti;
 
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.time.LocalDate;
 
 public class Biglietto {
 
 	private int km, eta;
+	private LocalDate data;
+	private boolean flessibile;
 	
 	private final BigDecimal COSTO_PER_KM = new BigDecimal(0.21);
 	private final BigDecimal SCONTO_OVER = new BigDecimal(0.4);
 	private final BigDecimal SCONTO_UNDER = new BigDecimal(0.2);
+	private final BigDecimal EXTRA = new BigDecimal(0.1);
+	private final int DURATA_NORMALE = 30;
+	private final int DURATA_FLESSIBILE = 90;
 	 
-	public Biglietto(int km, int eta) {
+	public Biglietto(int km, int eta, LocalDate data, boolean flessibile) {
 		this.km = km;
 		this.eta = eta;
+		this.data = data;
+		this.flessibile = flessibile;
 	}
 	
 	 public boolean isValidKm() throws Exception {
@@ -43,7 +51,18 @@ public class Biglietto {
 		 BigDecimal kmBD = new BigDecimal(km);
 		 BigDecimal costo = COSTO_PER_KM.multiply(kmBD);
 		 BigDecimal sconto = costo.multiply(calcolaSconto());
-		 return costo.subtract(sconto,context);
+		 BigDecimal extra = costo.multiply(EXTRA);
+		 if (!flessibile) {
+			 return costo.subtract(sconto,context);
+		 }
+		 return costo.subtract(sconto).add(extra, context); 
+	 }
+	 
+	 public LocalDate calcolaDataScadenza() {
+		 if (!flessibile) {
+			 return LocalDate.now().plusDays(DURATA_NORMALE);
+		 }
+		 return LocalDate.now().plusDays(DURATA_FLESSIBILE);
 	 }
 
 }
